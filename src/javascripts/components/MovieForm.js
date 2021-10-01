a26ffd1b5078a1468a031d0722b93b884cc55428
Nikolaus Brinkerhoff <nbrinkerhoff@mail.weber.edu>
2021-10-01 16:13:20 -0600

    Final Submission

--- src/javascripts/components/MovieForm.js
+++ src/javascripts/components/MovieForm.js
@@ -8,9 +8,17 @@ export function MovieForm() {
     let {mid} = useParams()
 
     let movie = mid ? movies.find( m => m.id == mid) : {}
+    let [id, setID] = useState(movie.id)
     let [title, setTitle] = useState(movie.title)
     let [poster, setPoster] = useState(movie.poster)
     let [plot, setPlot] = useState(movie.plot)
+    let [year, setYear] = useState(movie.year)
+    let [rated, setRated] = useState(movie.rated)
+    let [genre, setGenre] = useState(movie.genre)
+    let [rating, setRating] = useState(movie.rating)
+    let [votes, setVotes] = useState(movie.votes)
+    let [imdbID, setImdbID] = useState(movie.imdbID)
+    let [reviews, setReviews] = useState(movie.reviews)
     const history = useHistory()
     const submit = e => {
         e.preventDefault();
@@ -25,7 +33,6 @@ export function MovieForm() {
                     <p className="help">Title is required</p>
                 </div>
             </div>
-
             <div className="field">
                 <label htmlFor="poster">Poster</label>
                 <div className="control">
@@ -33,7 +40,6 @@ export function MovieForm() {
                     <p className="help">Poster is required</p>
                 </div>
             </div>
-
             <div className="field">
                 <label htmlFor="plot">Plot</label>
                 <div className="control">
@@ -41,8 +47,62 @@ export function MovieForm() {
                     <p className="help">Plot is required</p>
                 </div>
             </div>
-
-
+            <div className="field">
+                <label htmlFor="id">ID</label>
+                <div className="control">
+                    <input type="text" name="id" value={id} onChange={e => setID(e.target.value)}/>
+                    <p className="help">ID is required</p>
+                </div>
+            </div>
+            <div className="field">
+                <label htmlFor="year">Year</label>
+                <div className="control">
+                    <input type="text" name="year" value={year} onChange={e => setYear(e.target.value)}/>
+                    <p className="help">Year is required</p>
+                </div>
+            </div>
+            <div className="field">
+                <label htmlFor="rated">Rated</label>
+                <div className="control">
+                    <input type="text" name="rated" value={rated} onChange={e => setRated(e.target.value)}/>
+                    <p className="help">Rated is required</p>
+                </div>
+            </div>
+            <div className="field">
+                <label htmlFor="genre">Genre</label>
+                <div className="control">
+                    <input type="text" name="genre" value={genre} onChange={e => setGenre(e.target.value)}/>
+                    <p className="help">Genre is required</p>
+                </div>
+            </div>
+            <div className="field">
+                <label htmlFor="rating">Rating</label>
+                <div className="control">
+                    <input type="text" name="rating" value={rating} onChange={e => setRating(e.target.value)}/>
+                    <p className="help">Rating is required</p>
+                </div>
+            </div>
+            <div className="field">
+                <label htmlFor="votes">Votes</label>
+                <div className="control">
+                    <input type="text" name="votes" value={votes} onChange={e => setVotes(e.target.value)}/>
+                    <p className="help">Votes is required</p>
+                </div>
+            </div>
+            <div className="field">
+                <label htmlFor="imdbID">imdbID</label>
+                <div className="control">
+                    <input type="text" name="imdbID" value={imdbID} onChange={e => setImdbID(e.target.value)}/>
+                    <p className="help">imdbID is required</p>
+                </div>
+            </div>
+            <div className="field">
+                <label htmlFor="Reviews">Reviews</label>
+                <div className="control">
+                    <input type="text" name="reviews" value={reviews} onChange={e => setReviews(e.target.value)}/>
+                    <p className="help">Reviews is required</p>
+                </div>
+            </div>
 
             {/* Submit and Cancel Buttons */}
             <div className="field">
